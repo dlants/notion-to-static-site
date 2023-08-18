842109fa5cddc5e4852ff6211d6607170333c688
Denis Lantsman <dlantsman@amplify.com>
2023-08-17 20:34:10 -0700

    start to add header styling

--- transforms/insert-header.tsx
+++ transforms/insert-header.tsx
@@ -2,6 +2,8 @@ import * as cheerio from "cheerio";
 import * as React from "react";
 import { renderToString } from "react-dom/server";
 import { PageInfo } from "../util";
+import { stylesheet, getStyles } from "typestyle";
+import * as csstips from "csstips";
 
 export function insertHeader({
   $,
@@ -12,30 +14,50 @@ export function insertHeader({
   page: PageInfo;
   sectionPages: PageInfo[];
 }) {
+  const css = stylesheet({
+    breadcrumbContainer: {
+      ...csstips.flexRoot,
+    },
+    sectionsContainer: {
+      ...csstips.flexRoot,
+    },
+    breadcrumb: {
+      ...csstips.content,
+    },
+    section: {
+      ...csstips.content,
+    },
+    subscribe: {
+      ...csstips.content,
+    },
+  });
+
+  $("head").append($(`<style>${getStyles()}</style>`));
+
   $("header").prepend(
     renderToString(
       <div className="nav-header">
-        <div className="breadcrumbs">
+        <div className={css.breadcrumbContainer}>
           {page.breadcrumbs.map((breadcrumb, idx) => (
-            <div className="breadcrumb">
+            <div className={css.breadcrumb}>
               {idx == 0 ? "" : " > "}
               <a href={breadcrumb.url}>{breadcrumb.title}</a>
             </div>
           ))}
-          <div className="breadcrumb">
+          <div className={css.breadcrumb}>
             {" > "}
             <a href={page.newPath}>{page.title}</a>
           </div>
         </div>
 
-        <div className="sections">
+        <div className={css.sectionsContainer}>
           {sectionPages.map((page) => (
-            <div className="section-link">
+            <div className={css.section}>
               <a href={page.newPath}>{page.title}</a>
             </div>
           ))}
-          <div className="">
-            <a href="https://buttondown.email/dlants">email signup</a>
+          <div className={css.subscribe}>
+            <a href="https://buttondown.email/dlants">get emails</a>
           </div>
         </div>
       </div>,
