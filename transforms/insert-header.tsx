0a8322a28baabdce02f1df003bb223327f51b306
Denis Lantsman <dlantsman@amplify.com>
2023-08-19 10:26:36 -0700

    fixup header flex

--- transforms/insert-header.tsx
+++ transforms/insert-header.tsx
@@ -2,7 +2,7 @@ import * as cheerio from "cheerio";
 import * as React from "react";
 import { renderToString } from "react-dom/server";
 import { PageInfo } from "../util";
-import { stylesheet, getStyles } from "typestyle";
+import { stylesheet, getStyles, classes } from "typestyle";
 import * as csstips from "csstips";
 
 export function insertHeader({
@@ -15,50 +15,43 @@ export function insertHeader({
   sectionPages: PageInfo[];
 }) {
   const css = stylesheet({
-    breadcrumbContainer: {
-      ...csstips.flexRoot,
+    navHeader: {
+      ...csstips.pageTop,
+      ...csstips.horizontal,
+      ...csstips.horizontallySpaced(10),
     },
-    sectionsContainer: {
-      ...csstips.flexRoot,
-    },
-    breadcrumb: {
+    headerItem: {
       ...csstips.content,
     },
-    section: {
-      ...csstips.content,
+    divider: {
+      ...csstips.flex,
     },
-    subscribe: {
-      ...csstips.content,
-    },
+    subscribe: {},
   });
 
   $("head").append($(`<style>${getStyles()}</style>`));
 
   $("header").prepend(
     renderToString(
-      <div className="nav-header">
-        <div className={css.breadcrumbContainer}>
-          {page.breadcrumbs.map((breadcrumb, idx) => (
-            <div className={css.breadcrumb}>
-              {idx == 0 ? "" : " > "}
-              <a href={breadcrumb.url}>{breadcrumb.title}</a>
-            </div>
-          ))}
-          <div className={css.breadcrumb}>
-            {" > "}
+      <div className={css.navHeader}>
+        {page.breadcrumbs.map((breadcrumb, idx) => (
+          <div className={css.headerItem}>
+            {idx == 0 ? "" : ">"}
+            <a href={breadcrumb.url}>{breadcrumb.title}</a>
+          </div>
+        ))}
+        <div className={css.headerItem}>
+          {"> "}
+          <a href={page.pageUrl}>{page.title}</a>
+        </div>
+        <div className={css.divider} />
+        {sectionPages.map((page) => (
+          <div className={css.headerItem}>
             <a href={page.pageUrl}>{page.title}</a>
           </div>
-        </div>
-
-        <div className={css.sectionsContainer}>
-          {sectionPages.map((page) => (
-            <div className={css.section}>
-              <a href={page.pageUrl}>{page.title}</a>
-            </div>
-          ))}
-          <div className={css.subscribe}>
-            <a href="https://buttondown.email/dlants">get emails</a>
-          </div>
+        ))}
+        <div className={classes(css.headerItem, css.subscribe)}>
+          <a href="https://buttondown.email/dlants">get emails</a>
         </div>
       </div>,
     ),
